59e9a791725ab6ff94882492c09abad1d5e5f4b7
wangzhongren <wangzhongren@sjtu.edu.cn>
2023-03-19 21:16:29 +0800

    add some comments for BC

--- algos/BC/config.py
+++ algos/BC/config.py
@@ -1,7 +1,7 @@
 class AlgoConfig:
     def __init__(self):
-        self.continuous = False # continuous action space
-        self.lr = 0.0003 # learning rate for actor
-        self.batch_size = 128 # ppo train batch size
-        self.train_iterations = 200 # the iterarions of train
-        self.actor_hidden_dim = 256 # hidden dimension for actor
+        self.continuous = False # 连续状态空间
+        self.lr = 0.0003 # 学习率
+        self.batch_size = 128 # batch大小
+        self.train_iterations = 500 # 训练的迭代次数
+        self.actor_hidden_dim = 256 # 隐藏层维度
