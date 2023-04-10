06d6a06bf415e24147a6c6efd7436ce84a1cd9b3
“katniss1025” <819147806@qq.com>
2023-04-10 09:27:40 +0800

    DoubleDQN增加中文注释

--- algos/DoubleDQN/agent.py
+++ algos/DoubleDQN/agent.py
@@ -23,49 +23,66 @@ import numpy as np
 from common.memories import ReplayBufferQue
 from common.layers import ValueNetwork
 
+
 class Agent:
-    def __init__(self,cfg):
+    def __init__(self, cfg):
+        '''智能体类
+        Args:
+            cfg (class): 超参数类
+        '''
         self.n_actions = cfg.n_actions
         self.device = torch.device(cfg.device) 
-        self.gamma = cfg.gamma  
+        self.gamma = cfg.gamma  # 折扣因子
         ## e-greedy parameters
         self.sample_count = 0  # sample count for epsilon decay
         self.epsilon_start = cfg.epsilon_start
         self.epsilon_end = cfg.epsilon_end
         self.epsilon_decay = cfg.epsilon_decay
         self.batch_size = cfg.batch_size
-        self.target_update = cfg.target_update
-        self.policy_net = ValueNetwork(cfg).to(self.device)
+        self.target_update = cfg.target_update  # 目标网络更新频率
+        self.policy_net = ValueNetwork(cfg).to(self.device)  # Q网络
         # summary(self.policy_net, (1,4))
-        self.target_net = ValueNetwork(cfg).to(self.device)
+        self.target_net = ValueNetwork(cfg).to(self.device)  # 目标网络
         # target_net copy from policy_net
         for target_param, param in zip(self.target_net.parameters(), self.policy_net.parameters()):
             target_param.data.copy_(param.data)
         # self.target_net.eval()  # donnot use BatchNormalization or Dropout
         # the difference between parameters() and state_dict() is that parameters() require_grad=True
-        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=cfg.lr)
-        self.memory = ReplayBufferQue(cfg.buffer_size)
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=cfg.lr)  # 使用Adam优化器
+        self.memory = ReplayBufferQue(cfg.buffer_size)  # 经验回放池
         self.update_flag = False 
         
     def sample_action(self, state):
-        ''' sample action
+        '''采样动作
+        Args:
+            state(array): 状态
+        Returns:
+            action(int): 动作
         '''
         self.sample_count += 1
         self.epsilon = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * math.exp(-1. * self.sample_count / self.epsilon_decay)
-        if random.random() > self.epsilon:
+        if random.random() > self.epsilon:  # 使用 epsilon greedy
             action = self.predict_action(state)  
         else:
             action = random.randrange(self.n_actions)
         return action
+
     def predict_action(self, state):
-        ''' predict action
+        ''' 预测动作
+        Args:
+            state(array): 状态
+        Returns:
+            actions(int): 动作
         '''
         with torch.no_grad():
             state = torch.tensor(state, device=self.device, dtype=torch.float32).unsqueeze(0)
             q_value = self.policy_net(state)
             action = q_value.max(1)[1].item()  
         return action
+
     def update(self):
+        '''更新网络参数
+        '''
         if len(self.memory) < self.batch_size: # when transitions in memory donot meet a batch, not update
             return
         else:
@@ -80,33 +97,42 @@ class Agent:
         reward_batch = torch.tensor(reward_batch, device=self.device, dtype=torch.float).unsqueeze(1)  # shape(batchsize,1)
         next_state_batch = torch.tensor(np.array(next_state_batch), device=self.device, dtype=torch.float)
         done_batch = torch.tensor(np.float32(done_batch), device=self.device).unsqueeze(1) # shape(batchsize,1)
-        # 计算当前Q(s_t|a=a_t)，即Q网络的输出，这里的gather函数的作用是根据action_batch中的值，从Q网络的输出中选出对应的Q值
+        # 计算当前Q(s_t, a_t)，即Q网络的输出，这里的gather函数的作用是根据action_batch中的值，从Q网络的输出中选出对应的Q值
         q_value_batch = self.policy_net(state_batch).gather(dim=1, index=action_batch) # shape(batchsize,1)
-        # 计算 Q(s_t+1|a)
+        # Q网络计算 Q(s_t+1, a)
         next_q_value_batch = self.policy_net(next_state_batch)
-        # 计算 Q'(s_t+1|a)，也是与DQN不同的地方
+        # 目标网络计算 Q'(s_t+1, a)，也是与DQN不同的地方
         next_target_value_batch = self.target_net(next_state_batch)
-        # 计算 Q'(s_t+1|a=argmax Q(s_t+1|a))
+        # 计算 Q'(s_t+1, a=argmax Q(s_t+1, a))
         next_target_q_value_batch = next_target_value_batch.gather(1, torch.max(next_q_value_batch, 1)[1].unsqueeze(1)) # shape(batchsize,1)
-        expected_q_value_batch  = reward_batch + self.gamma * next_target_q_value_batch * (1-done_batch)
-        loss = nn.MSELoss()(q_value_batch , expected_q_value_batch)  
-        self.optimizer.zero_grad()  
-        loss.backward()
+        expected_q_value_batch = reward_batch + self.gamma * next_target_q_value_batch * (1-done_batch)  # TD误差目标
+        loss = nn.MSELoss()(q_value_batch, expected_q_value_batch)  # 均方误差损失函数
+        self.optimizer.zero_grad()  # Pytorch 默认梯度会累计，这里需要显式将梯度设置为0
+        loss.backward()  # 反向传播更新参数
         # clip to avoid gradient explosion
         for param in self.policy_net.parameters(): 
             param.grad.data.clamp_(-1, 1)
         self.optimizer.step()  
 
-        if self.sample_count % self.target_update == 0: # target net update, target_update means "C" in pseucodes
+        # 目标网络参数不频繁更新，而是定期从Q网络复制过来，这样有助于提升训练的稳定性和收敛性
+        if self.sample_count % self.target_update == 0:  # target net update, target_update means "C" in pseudocode
             self.target_net.load_state_dict(self.policy_net.state_dict()) 
     
-    def save_model(self,path):
+    def save_model(self, path):
+        '''保存模型
+        Args:
+            path(str): 存储路径
+        '''
         from pathlib import Path
         # create path
         Path(path).mkdir(parents=True, exist_ok=True)
         torch.save(self.target_net.state_dict(), f"{path}/checkpoint.pth")
 
-    def load_model(self,path):
+    def load_model(self, path):
+        '''加载模型
+        Args:
+            path(str): 加载路径
+        '''
         self.target_net.load_state_dict(torch.load(f"{path}/checkpoint.pth"))  
         for target_param, param in zip(self.target_net.parameters(), self.policy_net.parameters()):
             param.data.copy_(target_param.data)  
--- algos/DoubleDQN/trainer.py
+++ algos/DoubleDQN/trainer.py
@@ -8,36 +8,66 @@ LastEditor: JiangJi
 LastEditTime: 2023-04-05 01:17:26
 Discription: 
 '''
+
+
 class Trainer:
+    '''训练类
+    '''
     def __init__(self) -> None:
         pass
-    def train_one_episode(self, env, agent, cfg): 
+
+    def train_one_episode(self, env, agent, cfg):
+        '''定义一个回合的训练
+        Args:
+            env(class): 环境类
+            agent(class): 智能体类
+            cfg(class): 超参数类
+        Returns:
+            agent(class):智能体类
+            res(dict): 一个回合的结果 keys={'ep_reward', 'ep_step'}
+                ep_reward(float): 一个回合获得的回报
+                ep_step(int): 一个回合总步数
+        '''
         ep_reward = 0  # reward per episode
         ep_step = 0
         state = env.reset()  # reset and obtain initial state
         for _ in range(cfg.max_steps):
-            ep_step += 1
+            ep_step += 1  # 时间步
             action = agent.sample_action(state)  # sample action
-            next_state, reward, terminated, truncated , info = env.step(action)  # update env and return transitions under new_step_api of OpenAI Gym
-            agent.memory.push((state, action, reward,next_state, terminated))  # save transitions
+            next_state, reward, terminated, truncated, info = env.step(
+                action)  # update env and return transitions under new_step_api of OpenAI Gym
+            agent.memory.push((state, action, reward, next_state, terminated))  # save transitions
             agent.update()  # update agent
             state = next_state  # update next state for env
             ep_reward += reward  #
-            if terminated:
+            if terminated:  # 回合结束
                 break
-        res = {'ep_reward':ep_reward,'ep_step':ep_step}
-        return agent,res
+        res = {'ep_reward': ep_reward, 'ep_step': ep_step}  # ep_reward:一个回合获得的回报, ep_step:一个回合总步数
+        return agent, res
+
     def test_one_episode(self, env, agent, cfg):
+        '''定义一个回合的测试
+        Args:
+            env(class): 环境类
+            agent(class): 智能体类
+            cfg(class): 超参数类
+        Returns:
+            agent(class):智能体类
+            res(dict): 一个回合的结果 keys={'ep_reward', 'ep_step'}
+                ep_reward(float): 一个回合获得的回报
+                ep_step(int): 一个回合总步数
+        '''
         ep_reward = 0  # reward per episode
         ep_step = 0
         state = env.reset()  # reset and obtain initial state
         for _ in range(cfg.max_steps):
-            ep_step += 1
+            ep_step += 1  # 时间步
             action = agent.predict_action(state)  # sample action
-            next_state, reward, terminated, truncated , info = env.step(action)  # update env and return transitions under new_step_api of OpenAI Gym
+            next_state, reward, terminated, truncated, info = env.step(
+                action)  # update env and return transitions under new_step_api of OpenAI Gym
             state = next_state  # update next state for env
             ep_reward += reward  #
-            if terminated:
+            if terminated:  # 回合结束
                 break
-        res = {'ep_reward':ep_reward,'ep_step':ep_step}
-        return agent,res
+        res = {'ep_reward': ep_reward, 'ep_step': ep_step}
+        return agent, res
