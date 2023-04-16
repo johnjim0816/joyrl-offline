6200b89bcecd9108d93f368c71661dc3d3f0d3f1
WBYZ777 <92857635+WBYZ777@users.noreply.github.com>
2023-04-16 16:15:14 +0800

    Sarsa的中文注释

--- algos/Sarsa/trainer.py
+++ algos/Sarsa/trainer.py
@@ -12,41 +12,61 @@ class Trainer:
     def __init__(self) -> None:
         pass
     def train_one_episode(self, env, agent, cfg): 
-        ep_reward = 0  # reward per episode
+        '''
+        更新一轮参数
+        Args:
+            env (gym): 输入的env实例
+            agent (class): 输入的agent实例
+            cfg (class): 超参数配置实例
+        Returns:
+            agent (class): 更新一轮参数后的agent实例
+            res (dict): 更新一轮后的总奖励值及更新的step总数
+        '''
+        ep_reward = 0  # 一轮的累计奖励 
         ep_step = 0
-        state = env.reset(seed = cfg.seed)  # reset and obtain initial state
+        state = env.reset(seed = cfg.seed)  # 重置环境并获取初始状态,即开始新的回合
         for _ in range(cfg.max_steps):
             ep_step += 1
-            action = agent.sample_action(state)  # sample action
+            action = agent.sample_action(state) # 采样动作 
             if cfg.new_step_api:
-                next_state, reward, terminated, truncated , info = env.step(action)  # update env and return transitions under new_step_api of OpenAI Gym
+                next_state, reward, terminated, truncated , info = env.step(action)  # 更新环境并返回新状态、奖励、终止状态、截断标志和其他信息（使用 OpenAI Gym 的 new_step_api）
             else:
-                next_state, reward, terminated, info = env.step(action)  # update env and return transitions under old_step_api of OpenAI Gym
+                next_state, reward, terminated, info = env.step(action)  # 更新环境并返回新状态、奖励、终止状态、截断标志和其他信息（使用 OpenAI Gym 的 new_step_api）
             next_action =  agent.sample_action(next_state)
-            agent.update(state, action, reward, next_state, next_action,terminated)  # update agent
-            state = next_state  # update next state for env
-            action = next_action # update next action
-            ep_reward += reward  # accumulate reward
+            agent.update(state, action, reward, next_state, next_action,terminated)  # 更新 agent
+            state = next_state   # 更新状态 
+            action = next_action # 更新状态 
+            ep_reward += reward  # 累积奖励
             if terminated:
                 break
         res = {'ep_reward':ep_reward,'ep_step':ep_step}
         return agent,res
     
     def test_one_episode(self, env, agent, cfg):
-        ep_reward = 0  # reward per episode
+        '''
+        预测一轮
+        Args:
+            env (gym): 输入的env实例
+            agent (class): 输入的agent实例
+            cfg (class): 超参数配置实例
+        Returns:
+            agent (class): 执行完一轮后的agent实例
+            res (dict): 执行一轮后的总奖励值及预测的step总数
+        '''
+        ep_reward = 0  # 一轮的累计奖励 
         ep_step = 0
-        state = env.reset(seed = cfg.seed)  # reset and obtain initial state
+        state = env.reset(seed = cfg.seed)   # 重置环境并获取初始状态,即开始新的回合
         for _ in range(cfg.max_steps):
             if cfg.render:
                 env.render()
             ep_step += 1
-            action = agent.predict_action(state)  # sample action
+            action = agent.predict_action(state)  # 预测动作 
             if cfg.new_step_api:
-                next_state, reward, terminated, truncated , info = env.step(action)  # update env and return transitions under new_step_api of OpenAI Gym
+                next_state, reward, terminated, truncated , info = env.step(action)  # 更新环境并返回新状态、奖励、终止状态、截断标志和其他信息（使用 OpenAI Gym 的 new_step_api）
             else:
-                next_state, reward, terminated, info = env.step(action) # update env and return transitions under old_step_api of OpenAI Gym
-            state = next_state  # update next state for env
-            ep_reward += reward  #
+                next_state, reward, terminated, info = env.step(action) # 更新环境并返回新状态、奖励、终止状态、截断标志和其他信息（使用 OpenAI Gym 的 new_step_api）
+            state = next_state   # 更新状态 
+            ep_reward += reward  # 增加奖励
             if terminated:
                 break
         res = {'ep_reward':ep_reward,'ep_step':ep_step}
