66305e8cb867ccf1c25232415be0a790660149ee
gsc579 <1935913776@qq.com>
2023-06-18 21:39:26 +0800

    update sac

--- algos/SAC/config.py
+++ algos/SAC/config.py
@@ -1,20 +1,18 @@
 class AlgoConfig:
     '''算法超参数类
     '''
-    def __init__(self) -> None:
-        # self.policy_type = 'Gaussian' # 策略类型                        
-        self.critic1_lr = 1e-3 # critic1的学习率 # 3e-4 
-        self.critic2_lr = 1e-3 # 学习率 # 3e-4 
-        self.actor_lr = 3e-4  # 动作网络学习率 # 3e-4 
-        self.alpha_lr = 1e-4                   
-        self.gamma = 0.99 # 折扣因子                                       
-        self.tau = 0.005 # 软更新因子                                      
-        self.alpha = 0.1 # 温度参数 α 决定信息熵相对于奖励的重要性 # 0.1   
-        self.automatic_entropy_tuning = False # 自动调整 α                  
-        self.batch_size = 64 # 批次大小 # 256                                
-        self.hidden_dim = 64 # 隐藏层维度 # 256    
-        self.n_epochs = 1 # 回合数                 
-        self.start_steps = 10000 # 利用前的探索步数                
-        self.target_update_fre = 1 # 更新目标网络的时间间隔  
-        self.buffer_size = 1000000 # 经验回放池的大小   
+    def __init__(self) -> None:                     
+        self.critic1_lr = 1e-3 # critic1 learning rate 
+        self.critic2_lr = 1e-3 # critic2 learning rate 
+        self.actor_lr = 3e-4  # actor learning rate 
+        self.alpha_lr = 1e-4  # alpha learning rate                 
+        self.gamma = 0.99 # reward discount factor                                       
+        self.tau = 0.005 # soft update factor                                     
+        self.batch_size = 64 # batch size                             
+        self.hidden_dim = 64 # hidden dimension  
+        self.n_epochs = 1 # number of epochs                 
+        self.start_steps = 10000 # number of exploration              
+        self.target_update_fre = 1 # target net update frequent  
+        self.buffer_size = 1000000 # buffer size 
         self.min_policy = 0 # min value for policy (for discrete action space)      
+        self.buffer_type = "REPLAY_QUE" # buffer type
