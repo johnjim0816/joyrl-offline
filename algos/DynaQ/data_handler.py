b01da7954b2dcd525cfb24d95f94e4f456ecbe79
limafang <21009201106@stu.xidian.edu.cn>
2023-06-21 22:13:31 +0800

    更改了DynaQ算法结构，适应新框架

--- algos/DynaQ/data_handler.py
+++ algos/DynaQ/data_handler.py
@@ -1,25 +1,23 @@
+
 import numpy as np
 from algos.base.data_handlers import BaseDataHandler
 from algos.base.exps import Exp
-
-
 class DataHandler(BaseDataHandler):
-    def __init__(self, cfg) -> None:
+    def __init__(self,cfg) -> None:
         self.cfg = cfg
         self.buffer = []
         self.data_after_train = {}
-
     def add_transition(self, transition):
         ''' add transition to buffer
         '''
         exp = self._create_exp(transition)
         self.buffer.append(exp)
-
-    def add_data_after_train(self, data):
+    def add_exps(self, exps):
+        self.buffer.append(exps)
+    def add_data_after_learn(self, data):
         ''' add update data
         '''
         self.data_after_train = data
-
     def sample_training_data(self):
         ''' sample training data from buffer
         '''
@@ -28,12 +26,10 @@ class DataHandler(BaseDataHandler):
             return self.handle_exps_before_train(exp)
         else:
             return None
-
-    def _create_exp(self, transtion):
+    def _create_exp(self,transtion):
         ''' create experience
         '''
         return [Exp(**transtion)]
-
     def handle_exps_before_train(self, exp, **kwargs):
         ''' convert exps to training data
         '''
@@ -42,11 +38,10 @@ class DataHandler(BaseDataHandler):
         reward = np.array(exp.reward)
         next_state = np.array(exp.next_state)
         done = np.array(exp.done)
-        data = {'state': state, 'action': action, 'reward': reward,
-                'next_state': next_state, 'done': done}
+        data = {'state': state, 'action': action, 'reward': reward, 'next_state': next_state, 'done': done}
         return data
-
     def handle_exps_after_train(self):
         ''' handle exps after train
         '''
         pass
+    
